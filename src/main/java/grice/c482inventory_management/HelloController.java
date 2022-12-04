fbe6e2a1a5aa055ef41e6f6972ba5545aecc8ce6
actual <actual@BIGSKY.localdomain>
2022-12-04 12:49:08 -0600

    Initial commit

---
+++ src/main/java/grice/c482inventory_management/HelloController.java
@@ -0,0 +1,14 @@
+package grice.c482inventory_management;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module grice.c482inventory_management {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens grice.c482inventory_management to javafx.fxml;
+    exports grice.c482inventory_management;
+}
